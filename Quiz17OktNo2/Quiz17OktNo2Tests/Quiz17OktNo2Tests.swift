26f73eb63377c71b785228657a9b8c8bd4692d26
Programmer Muslim <rizki@imastudio.co.id>
2017-10-17 08:05:29 +0700

    quiz

---
+++ Quiz17OktNo2/Quiz17OktNo2Tests/Quiz17OktNo2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Quiz17OktNo2Tests.swift
+//  Quiz17OktNo2Tests
+//
+//  Created by Rizki Syaputra on 10/17/17.
+//  Copyright © 2017 Rizki Syaputra. All rights reserved.
+//
+
+import XCTest
+@testable import Quiz17OktNo2
+
+class Quiz17OktNo2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
